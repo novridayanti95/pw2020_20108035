4c3a9a5e9339477f6d5dcdc0c18a6a715aa623e1
unknown <novridayanti.nd@gmail.com>
2024-02-23 14:00:13 +0700

    memperbaharui data

--- kuliah/pertemuan12/login.php
+++ kuliah/pertemuan12/login.php
@@ -44,6 +44,9 @@ if (isset($_POST['login'])) {
       <li>
         <button type="submit" name="login">Login</button>
       </li>
+      <li>
+        <a href="registrasi.php">Tambah User Baru!</a>
+      </li>
     </ul>
   </form>
 </body>
